2897fd733b9c7b6df59217382de2645dd66ab87d
Darshitvara <darshitvara2004@gmail.com>
2025-09-27 00:18:43 +0530

    bug fixed.

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://phonemax.darshitvara.me'], // allowed origins
-  methods: ['GET','POST','PUT','DELETE'],
-  // credentials: true // if using cookies/auth
+  origin: 'https://phonemax.darshitvara.me', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
 };
+
 
 app.use(cors(corsOptions));
 
